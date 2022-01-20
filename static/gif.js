fe8ce759e69e361b847caf0e0bfde4cde8fc85cd
Fedesky25 <fedesky.guglielmi@gmail.com>
2022-01-20 21:33:09 +0100

    gif gen: calculation and painting separation

--- static/gif.js
+++ static/gif.js
@@ -6,9 +6,13 @@ const GIFEncoder = require("gif-encoder-2");
 const { createCanvas } = require("canvas");
 
 
-const FRAMES = 120;
-const PARTICLES = 10;
+const FRAMES = 180;
+const PARTICLES = 12000;
 const TAU = 3;
+const CLR_NUM = 8;
+
+const WIDTH = 900; // 3440
+const HEIGHT = 600; // 1440
 
 /**
  * @param {Complex} x 
@@ -18,43 +22,104 @@ const TAU = 3;
     y.eq(x).mul(x).exponentiate();
 }
 
-const gif = new GIFEncoder(600, 400, "neuquant", false, FRAMES);
+
+const rand = (min, max) => Math.random()*(max-min)+min;
+const xScale = linearScale().domain(-3,3).range(0,WIDTH);
+const yScale = linearScale().domain(-2,2).range(HEIGHT,0);
+
+/**@returns {Any[][][]} */
+function newFrames() {
+    let i;
+    let j;
+    const res = new Array(FRAMES);
+    for(i=0; i<FRAMES; i++) {
+        res[i] = new Array(CLR_NUM);
+        for(j=0; j<CLR_NUM; j++) res[i][j] = [];
+    }
+    return res;
+}
+
+let start;
+const frames = newFrames();
+
+const x = new Complex();
+const y = new Complex();
+function particle() {
+    let i;
+    let factor;
+    const bias = Math.floor(Math.random()*FRAMES);
+    x.becomes(rand(-3,3), rand(-2,2));
+    for(i=bias; i<FRAMES; i++) {
+        f(x,y);
+        factor = -Math.expm1(-Math.sqrt(y.real*y.real+y.imag*y.imag)/TAU);
+        y.normalize().mul_r(0.01*factor);
+        x.add(y);
+        frames[i][Math.floor(factor*CLR_NUM-0.01)].push(xScale(x.real), yScale(x.imag));
+    }
+    for(i=0; i<bias; i++) {
+        f(x,y);
+        factor = -Math.expm1(-Math.sqrt(y.real*y.real+y.imag*y.imag)/TAU);
+        y.normalize().mul_r(0.01*factor);
+        x.add(y);
+        frames[i][Math.floor(factor*CLR_NUM-0.01)].push(xScale(x.real), yScale(x.imag));
+    }
+}
+
+console.log("Particle motion:");
+start = performance.now();
+progress(PARTICLES, particle);
+console.log("Time elapsed: "+(performance.now()-start).toPrecision(3)+" ms");
+
+// const dz = new Complex();
+// /**@type {Complex[]} */
+// const z = new Array(PARTICLES);
+// for(var i=0; i<PARTICLES; i++) z[i] = new Complex(rand(-3,3), rand(-2,2));
+// function frame(fi) {
+//     let factor;
+//     for(var i=0; i<PARTICLES; i++) {
+//         f(z[i],dz);
+//         factor = -Math.expm1(-Math.sqrt(dz.real*dz.real+dz.imag*dz.imag)/TAU);
+//         dz.normalize().mul_r(0.01*factor);
+//         z[i].add(dz);
+//         frames[fi][Math.floor(factor*CLR_NUM-0.01)].push(xScale(z[i].real), yScale(z[i].imag));
+//     }
+// }
+
+// console.log("Frames calculation:");
+// start = performance.now();
+// progress(FRAMES, frame);
+// console.log("Time elapsed: "+(performance.now()-start).toPrecision(3)+" ms");
+
+const ctx = createCanvas(WIDTH, HEIGHT).getContext('2d');
+ctx.fillStyle = "#111";
+ctx.fillRect(0,0,WIDTH,HEIGHT);
+
+const gif = new GIFEncoder(WIDTH, HEIGHT, "neuquant", false, FRAMES);
 gif.setFrameRate(60);
 gif.setRepeat(0);
 
-const rand = (min, max) => Math.random()*(max-min)+min;
-const dz = new Complex();
-/**@type {Complex[]} */
-const z = new Array(PARTICLES);
-for(var i=0; i<PARTICLES; i++) z[i] = new Complex(rand(-3,3), rand(-2,2));
-
-const xScale = linearScale().domain(-3,3).range(0,600);
-const yScale = linearScale().domain(-2,2).range(400,0);
-const ctx = createCanvas(600, 400).getContext('2d');
-ctx.fillStyle = "#111";
-ctx.fillRect(0,0,600,400);
-
-function frame() {
-    ctx.fillStyle = "#1115";
-    ctx.fillRect(0,0,600,400);
-    let speed;
-    let factor;
-    for(var i=0; i<PARTICLES; i++) {
-        f(z[i], dz);
-        speed = Math.sqrt(dz.real*dz.real + dz.imag*dz.imag);
-        factor = Math.expm1(-speed/TAU) // [-1,0]
-        dz.normalize().mul_r(-0.01*factor);
-        z[i].add(dz);
-
-        ctx.fillStyle = "hsl("+240*(1+factor)+",55%,55%)";
-        ctx.fillRect(xScale(z[i].real), yScale(z[i].imag), 1, 1);
+function paint(fi) {
+    ctx.fillStyle = "#1114";
+    ctx.fillRect(0,0,WIDTH,HEIGHT);
+    const frame = frames[fi];
+    var i;
+    var j;
+    for(i=0; i<CLR_NUM; i++) {
+        ctx.fillStyle = "hsl("+240*(1-i/(CLR_NUM-1))+",55%,55%)";
+        ctx.beginPath();
+        for(j=frame[i].length-1; j>0; j-=2) ctx.rect(frame[i][j-1], frame[i][j], 1, 1);
+        ctx.fill();
     }
     gif.addFrame(ctx);
 }
 
+console.log("Frames painting:");
+start = performance.now();
 gif.start();
-progress(FRAMES, frame);
+progress(FRAMES, paint);
 gif.finish();
+console.log("Time elapsed: "+((performance.now()-start)*1e-3).toPrecision(3)+" s");
+
 writeFile('./prova.gif', gif.out.getData())
 .then(() => console.log("Done"))
 .catch(err => console.log(err));
--- static/progress.js
+++ static/progress.js
@@ -7,6 +7,16 @@ function display(n) {
     stdout.write(n+'%');
 }
 
+function progressAbort() {
+    stdout.write("\x1B[?25h\n");
+    console.log("Uncaugth exception while progressing");
+}
+
+/**
+ * Displays a progression bar while executing a function repeatdly
+ * @param {number} times number of times to repeat the function
+ * @param {function} fn function to execute
+ */
 function progress(times, fn) {
     let i;
     let j;
@@ -15,16 +25,17 @@ function progress(times, fn) {
     let inner = Math.ceil(times / 100);
     stdout.write("\x1B[?25l");
     stdout.write('['+'\u2591'.repeat(100)+'] 0%');
+    process.on("uncaughtExceptionMonitor", progressAbort);
     for(i=0; i<outer; i++) {
-        for(j=0; j<inner; j++) fn();
+        for(j=0; j<inner; j++) fn(i*inner+j);
         display(++cursor);
     }
     if(outer) inner -= 1;
-    outer = 100 - outer;
-    for(i=0; i<outer; i++) {
-        for(j=0; j<inner; j++) fn();
+    for(i=outer; i<100; i++) {
+        for(j=0; j<inner; j++) fn(i*inner+j+outer);
         display(++cursor);
     }
+    process.off("uncaughtExceptionMonitor", progressAbort);
     stdout.write("\x1B[?25h\n");
 }
 
